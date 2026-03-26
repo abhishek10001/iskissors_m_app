861ac439f9e119ed6effaa138fde2ec4210af7c4
M@$ter.. <129093355+abhishek10001@users.noreply.github.com>
2026-03-27 00:36:31 +0530

    firebase setup

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,18 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'core/theme/app_theme.dart';
-import 'core/router/app_router.dart';
 
-void main() {
+import 'core/router/app_router.dart';
+import 'core/theme/app_theme.dart';
+import 'firebase_options.dart';
+
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   GoogleFonts.config.allowRuntimeFetching = false;
   SystemChrome.setSystemUIOverlayStyle(
     const SystemUiOverlayStyle(
